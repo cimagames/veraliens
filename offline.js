e567eb4433c28491468ad7c8b86e2d033e728d35
santyafull <140284677+santyafull@users.noreply.github.com>
2024-02-20 12:00:26 -0300

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,21 +1,11 @@
 ﻿{
-	"version": 1708287852,
+	"version": 1708441053,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
-		"images/sprite-sheet0.png",
-		"images/sprite2-sheet0.png",
-		"images/sprite3-sheet0.png",
-		"images/sprite4-sheet0.png",
-		"images/fadein-sheet0.png",
-		"images/fadeout-sheet0.png",
-		"icon-16.png",
-		"icon-32.png",
-		"icon-114.png",
-		"icon-128.png",
-		"icon-256.png",
-		"loading-logo.png"
+		"images/particles.png",
+		"images/particles2.png"
 	]
 }
